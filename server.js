2ff4ec4c27b02e51db548b351d3d0b17208d878e
Samuel Tauil <samueltauil@gmail.com>
2024-12-11 16:03:29 -0500

    initial commit
    Signed-off-by: Samuel Tauil <samueltauil@gmail.com>

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var os = require("os");
+const PORT = 8080;
+const app = express();
+
+
+var healthy=true;
+
+
+app.get('/', function (req, res) {
+  res.send('Welcome App [version: 2 - ' + os.hostname() + ']\n');
+});
+
+
+
+app.get('/healthz', function (req, res) {
+  console.log('health check')
+  if(healthy)
+   res.send('OK');
+  else
+   res.status(404).send('NOT OK');
+});
+
+
+app.get('/kill', function (req, res) {
+   healthy=false;
+   res.send('Killed ' + os.hostname());
+});
+
+
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
+
+process.on('SIGINT', function () {
+    console.log('Cleanup.....');
+    process.exit();
+});
